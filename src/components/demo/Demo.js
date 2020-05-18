ffc2dff690832653ed81cfda04bf0092005d2814
Cale <caleworkman@gmail.com>
2020-05-18 15:13:21 -0400

    projects as a json file

--- src/components/demo/Demo.js
+++ src/components/demo/Demo.js
@@ -5,19 +5,15 @@ import "./Demo.css";
 class Demo extends Component {
 
   render() {
-
-    // TODO: Check if url includes https:///www. and if not, append it
-    const imageUrl = "https://www." + this.props.url + "/favicon.ico";
-
     return (
       <div className="demo">
         <Link to={"//"+this.props.url} target="_blank">
           <div className="demo__grid">
             <div className="demo__icon">
-              <img src={imageUrl} alt={null} />
+              <img src={this.props.imageUrl} alt={null} />
             </div>
             <h2 className="demo__title">
-              {this.props.title}
+              {this.props.name}
             </h2>
             <div className="demo__description">
               {this.props.description}
@@ -32,5 +28,5 @@ class Demo extends Component {
 export default Demo;
 
 Demo.defaultProps = {
-  imageUrl: "",
+  imageUrl: null,
 }
--- src/views/portfolio/Portfolio.js
+++ src/views/portfolio/Portfolio.js
@@ -3,36 +3,20 @@ import Demo from "../../components/demo/Demo.js";
 import Layout from "../layout/Layout.js";
 import "./Portfolio.css";
 
+const projects = require("./projects.json");
+
 class Portfolio extends Component {
   render() {
+
+    const projectComponents = Object.keys(projects).map(key => {
+      const project = projects[key];
+      return <Demo key={project.name} {...project} />
+    });
+
     return (
       <Layout>
         <div className="portfolio">
-          <Demo
-            title="In the Money"
-            description="Plot stock option profit-loss diagrams using real-time
-            Yahoo finance data. Developed mobile-first with a responsive web UI."
-            url="inthemoney.app"
-          />
-          <Demo
-            title="Barley"
-            description="An iOS mobile app for restaurant inventory management
-            and order submission. Written in Swift and using CloudKit API. A python
-            backend web-scrapes to keep the inventory up to date."
-            url="www.barleyapp.co"
-          />
-          <Demo
-            title="LifeChart"
-            description="A tool to generate an aesthetic personal timeline.
-            Written in React with Firebase backend."
-            url="www.lifechart.app"
-          />
-          <Demo
-            title="Wow Raid Bingo"
-            description="A game to be played inside the retail version of World
-            of Warcraft. Written in Lua using AceComm for player communication."
-            url="www.github.com/caleworkman/RaidBingo"
-          />
+          {projectComponents}
         </div>
       </Layout>
     );
