edb911266715c65fa76e4d6c51104561b931874d
Cale <caleworkman@gmail.com>
2020-05-18 14:51:25 -0400

    even layout for portfolio grid

--- src/components/demo/Demo.js
+++ src/components/demo/Demo.js
@@ -3,25 +3,28 @@ import {Link} from "react-router-dom"
 import "./Demo.css";
 
 class Demo extends Component {
+
   render() {
 
     // TODO: Check if url includes https:///www. and if not, append it
     const imageUrl = "https://www." + this.props.url + "/favicon.ico";
 
     return (
-      <Link to={"//"+this.props.url} target="_blank">
-        <div className="demo">
-          <div className="demo__icon">
-            <img src={imageUrl} alt={null} />
+      <div className="demo">
+        <Link to={"//"+this.props.url} target="_blank">
+          <div className="demo__grid">
+            <div className="demo__icon">
+              <img src={imageUrl} alt={null} />
+            </div>
+            <h2 className="demo__title">
+              {this.props.title}
+            </h2>
+            <div className="demo__description">
+              {this.props.description}
+            </div>
           </div>
-          <h2 className="demo__title">
-            {this.props.title}
-          </h2>
-          <div className="demo__description">
-            {this.props.description}
-          </div>
-        </div>
-      </Link>
+        </Link>
+      </div>
     )
   }
 }
