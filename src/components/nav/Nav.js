ac2c0237f46a498fba4089058bcb090ebce17e6b
Cale <caleworkman@gmail.com>
2020-05-18 22:07:19 -0400

    remove contact

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router";
 
+import {ReactComponent as AccountIcon} from "../../assets/account-circle.svg";
+import {ReactComponent as DescriptionIcon} from "../../assets/description.svg";
+import {ReactComponent as DevicesIcon} from "../../assets/devices.svg";
+import {ReactComponent as SchoolIcon} from "../../assets/school.svg";
+
+
 import "./Nav.css";
 
 class Nav extends Component {
@@ -8,13 +14,12 @@ class Nav extends Component {
     super(props);
     this.navigate = this.navigate.bind(this);
 
-    this.navs = [
-      'about',
-      'education',
-      'portfolio',
-      'contact',
-      'resume'
-    ];
+    this.navs = {
+      'about': <AccountIcon />,
+      'education': <SchoolIcon />,
+      'portfolio': <DevicesIcon />,
+      'resume': <DescriptionIcon />
+    };
   }
 
   navigate(event) {
@@ -24,13 +29,21 @@ class Nav extends Component {
   render() {
     const location = this.props.location.pathname.replace("/","");
 
-    const navs = this.navs.map(nav => {
+    const navs = Object.keys(this.navs).map(nav => {
       const className = (location === nav)
         ? "nav__option nav__option--selected"
         : "nav__option";
+
+      const icon = this.navs[nav];
+
       return (
         <div className={className} id={nav} key={nav} onClick={this.navigate}>
-          {nav}
+          <div className="nav__icon">
+            {icon}
+          </div>
+          <div className="nav__name">
+            {nav}
+          </div>
         </div>
       )}
     );
