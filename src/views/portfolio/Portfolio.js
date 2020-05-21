48ddf54eac0d46b37f7cc23f1a8bc75f2c4151ea
Cale <caleworkman@gmail.com>
2020-05-21 09:54:02 -0400

    rename project.json to portfolio.json

--- src/views/portfolio/Portfolio.js
+++ src/views/portfolio/Portfolio.js
@@ -3,12 +3,12 @@ import Demo from "../../components/demo/Demo.js";
 import Layout from "../layout/Layout.js";
 import "./Portfolio.css";
 
-const projects = require("./projects.json");
+const portfolio = require("./portfolio.json");
 
 class Portfolio extends Component {
   render() {
 
-    const projectComponents = Object.keys(projects).map(key => {
+    const projectComponents = Object.keys(portfolio).map(key => {
       const project = projects[key];
       return <Demo key={project.name} {...project} />
     });
