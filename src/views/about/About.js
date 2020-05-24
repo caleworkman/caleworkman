870c408efe22aba418619104f1791bc2925586fe
Cale <caleworkman@gmail.com>
2020-05-24 14:16:17 -0400

    security warnings noopener/noreferrer, add margin instead of padding

--- src/views/about/About.js
+++ src/views/about/About.js
@@ -21,11 +21,11 @@ class About extends Component {
           </p>
           <p>
             In my spare time, I develop mobile and web applications. I have
-            released <a href="https://www.barleyapp.co" target="_blank"><b>Barley</b></a>,
+            released <a href="https://www.barleyapp.co" target="_blank" rel="noopener noreferrer"><b>Barley</b></a>,
             an iOS mobile application designed to facilitate bar inventory management.
             The app is written in <b>Swift</b> with a <b>Python 3</b> backend.
             The app is currently serving bars throughout NC. I recently
-            released <a href="www.inthemoney.app" target="_blank"><b>In the Money App</b></a>,
+            released <a href="www.inthemoney.app" target="_blank" rel="noopener noreferrer"><b>In the Money App</b></a>,
             a <b>ReactJS</b> web application designed to build profit-loss diagrams
             from real-time stock option data.
           </p>
@@ -36,7 +36,7 @@ class About extends Component {
           <h3 className="about__subtitle">2018</h3>
           <p>
             I was a <b>C#/.NET</b> developer for the online form-building
-            software <a href="https://www.cognitoforms.com" target="_blank">Cognito Forms</a>. In addition
+            software <a href="https://www.cognitoforms.com" target="_blank" rel="noopener noreferrer">Cognito Forms</a>. In addition
             to feature development, I also handled Tier-3 customer support and
             was responsible for debugging complex customer forms and root-causing bugs.
           </p>
