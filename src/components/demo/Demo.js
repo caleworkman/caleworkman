ec763eca1ea34dceb77e9496a3fa4ab89bf895e7
Cale <caleworkman@gmail.com>
2020-05-25 23:36:13 -0400

    new links in portfolio, hardcode apple store icon background to gunmetal

--- src/components/demo/Demo.js
+++ src/components/demo/Demo.js
@@ -1,42 +1,67 @@
 import React, { Component } from "react";
 import Bubble from "../bubble/Bubble.js";
 import {Link} from "react-router-dom"
+
+import {ReactComponent as AppStoreIcon} from "../../assets/app-store.svg";
+import {ReactComponent as LaunchIcon} from "../../assets/launch.svg";
+import {ReactComponent as GitHubIcon} from "../../assets/github.svg";
+
 import "./Demo.css";
 
 class Demo extends Component {
 
   render() {
     return (
-      <Link to={"//" + this.props.url} target="_blank" className="demo__link">
-        <div className="demo">
-          <div className="demo__grid">
-            {this.props.imageUrl
-              ? <div className="demo__icon">
-                  <img src={this.props.imageUrl} alt={""} />
-                </div>
-              : null
-            }
-
-            <h1 className="demo__title">
-              {this.props.name}
-            </h1>
-            <p className="demo__description">
-              {this.props.description}
-            </p>
-
-            <div className="demo__tech">
-              {this.props.technologies.map(tech => {
-                return <Bubble content={tech} key={tech}/>
-              })}
-            </div>
-          </div>
-
-          {this.props.wip
-            ? <div className="demo__wip">*Under Development</div>
+      <div className="demo">
+        <div className="demo__grid">
+          {this.props.imageUrl
+            ? <div className="demo__icon">
+                <img src={this.props.imageUrl} alt={""} />
+              </div>
             : null
           }
+
+          <h1 className="demo__title">
+            {this.props.name}
+          </h1>
+
+          <div className="demo__links">
+            {this.props.appUrl
+              ? <Link to={"//" + this.props.appUrl} target="_blank">
+                  <AppStoreIcon className="demo__svg demo__svg--apple" />
+                </Link>
+              : null
+            }
+            {this.props.gitUrl
+              ? <Link to={"//" + this.props.gitUrl} target="_blank">
+                  <GitHubIcon className="demo__svg" />
+                </Link>
+              : null
+            }
+            {this.props.webUrl
+              ? <Link to={"//" + this.props.webUrl} target="_blank">
+                  <LaunchIcon className="demo__svg" />
+                </Link>
+              : null
+            }
+          </div>
+
+          <p className="demo__description">
+            {this.props.description}
+          </p>
+
+          <div className="demo__tech">
+            {this.props.technologies.map(tech => {
+              return <Bubble content={tech} key={tech}/>
+            })}
+          </div>
         </div>
-      </Link>
+
+        {this.props.wip
+          ? <div className="demo__wip">*Under Development</div>
+          : null
+        }
+      </div>
     )
   }
 }
